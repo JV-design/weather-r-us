a61c3a27437ddabd0f96cdb2f9e29180a078cd82
JV-design <jurie.venter13@gmail.com>
2025-03-27 15:58:37 +0200

    Final touches
    Updated logo
    clearFilter function
    Show More/Less function
    Button Loader
    Signup Form

--- login.js
+++ login.js
@@ -11,14 +11,3 @@ switchToLogin.addEventListener('click', function (e)  {
     $('#loginSection').toggleClass("d-none"); 
     $('#signupSection').toggleClass("d-none");       
 });
-
-
-
-function submitForm() {
-    document.getElementById("loginForm").submit()
-}
-
-document.getElementById('loginSubmit').onclick = function() {
-    this.innerHTML = "<i class='fa-solid fa-spinner fa-spin'></i>";
-    setTimeout(submitForm, 3000); 
-}
--- site.js
+++ site.js
@@ -1,18 +1,21 @@
-// Toolip
+// Normalise BS Toolip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-// Popover
+
+
+// Normalise BS Popover
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
 
-// Offcanvas trigger
+// Normalise BS Offcanvas trigger
 $('body').on( 'click tap', '.add-anomaly', function(e) {
 	var myOffcanvas = $('#offcanvasRight')[0];
     var bsOffcanvas = new bootstrap.Offcanvas(myOffcanvas);
     e.stopPropagation();
 	bsOffcanvas.toggle();  
 });
+
 
 //Toggle section icons
 let curSecToggle = document.querySelector('#currentSectionToggle');
@@ -25,12 +28,32 @@ curSecToggle.addEventListener('click', (e) =>  {
 });
 
 
+//Toggle Show More/Less
+let showMore = document.querySelector('#showMore');
+let showLess = document.querySelector('#showLess');
+
+showMore.addEventListener('click', (e) =>  {
+    $('#showMore').toggleClass("d-none"); 
+    $('#showLess').toggleClass("d-none");
+    $('#anomalyList').toggleClass("max-height");
+    $('#anomalyList').toggleClass("min-height");        
+});
+
+showLess.addEventListener('click', (e) =>  {
+    $('#showLess').toggleClass("d-none"); 
+    $('#showMore').toggleClass("d-none");
+    $('#anomalyList').toggleClass("min-height");
+    $('#anomalyList').toggleClass("max-height");        
+});
+
+
 // Get current date and time
 var now = new Date();
 var datetime = now.toISOString().slice(0, 10);
 
 // Insert date and time into HTML
 document.getElementById("datetime").innerHTML = datetime;
+
 
 // Declare Filter buttons
 let allFilterBtn = document.querySelector('#allFilterBtn');
@@ -41,9 +64,14 @@ let rainFilterBtn = document.querySelector('#rainFilterBtn');
 let thunderFilterBtn = document.querySelector('#thunderFilterBtn');
 let tornadoFilterBtn = document.querySelector('#tornadoFilterBtn');
 let droughtFilterBtn = document.querySelector('#droughtFilterBtn');
+let clearFilters = document.querySelector('#clearFilters');
+
 
 // Events for Filter buttons
 allFilterBtn.addEventListener('click', (e) =>  {
+    $('.hot-item, .cold-item, .wind-item, .rain-item, .thunder-item, .tornado-item, .drought-item').removeClass("d-none");       
+});
+clearFilters.addEventListener('click', (e) =>  {
     $('.hot-item, .cold-item, .wind-item, .rain-item, .thunder-item, .tornado-item, .drought-item').removeClass("d-none");       
 });
 coldFilterBtn.addEventListener('click', (e) =>  {
@@ -76,7 +104,7 @@ droughtFilterBtn.addEventListener('click', (e) =>  {
 });
 
 
-/* Filter Severity Function */
+// Filter Severity Function 
 document.querySelector("#severityRange").addEventListener('change', filterSeverity);
 
 function filterSeverity(){
@@ -101,3 +129,13 @@ function filterSeverity(){
     }  
 }
 
+// Add button loader 
+
+function submitForm() {
+    document.getElementById("anomalyForm").submit()
+}
+
+document.getElementById('addAnomaly').onclick = function() {
+    this.innerHTML = "<i class='fa-solid fa-spinner fa-spin'></i>";
+    setTimeout(submitForm, 3000);     
+}
